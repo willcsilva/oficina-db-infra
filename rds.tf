1a7a17d251a440fe212f1397f8be06dd618493cd
willcsilva <willcsilva15@gmail.com>
2026-01-31 06:28:33 -0300

    Ajustado outputs no arquivo main  DB Cluster

--- rds.tf
+++ rds.tf
@@ -48,3 +48,12 @@ resource "aws_db_instance" "oficina_db" {
     Project = "Oficina-api-13-soat"
   }
 }
+
+output "rds_endpoint" {
+  description = "Endpoint do banco de dados para a string de conexão"
+  value       = module.rds.db_instance_endpoint
+}
+
+output "rds_hostname" {
+  value = split(":", aws_db_instance.oficina_db.endpoint)[0]
+}
