94f12cd9c3bc105889f73ef596975856498a8c35
willcsilva <willcsilva15@gmail.com>
2026-01-31 06:21:28 -0300

    Subindo DB Cluster

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,7 @@ output "rds_endpoint" {
   description = "Endpoint do banco de dados para a string de conexão"
   value       = module.rds.db_instance_endpoint
 }
+
+output "rds_hostname" {
+  value = split(":", aws_db_instance.oficina_db.endpoint)[0]
+}
