5167a484ca3faa7863e1f0e2f7ba8ebd2bdcdafd
willcsilva <willcsilva15@gmail.com>
2026-01-31 06:29:44 -0300

    Removido outputs  DB Cluster

--- rds.tf
+++ rds.tf
@@ -49,10 +49,10 @@ resource "aws_db_instance" "oficina_db" {
   }
 }
 
-output "rds_endpoint" {
-  description = "Endpoint do banco de dados para a string de conexão"
-  value       = module.rds.db_instance_endpoint
-}
+# output "rds_endpoint" {
+#   description = "Endpoint do banco de dados para a string de conexão"
+#   value       = module.rds.db_instance_endpoint
+# }
 
 output "rds_hostname" {
   value = split(":", aws_db_instance.oficina_db.endpoint)[0]
