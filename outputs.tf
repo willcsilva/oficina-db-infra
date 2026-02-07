cf900f2093bfbf3ce3c62f6d31a7f510d72cbe24
willcsilva <willcsilva15@gmail.com>
2026-02-07 17:19:45 -0300

    Add output SNS

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,8 @@ output "db_name" {
   value = aws_db_instance.oficina_db.db_name
 }
 
+# Caminho: outputs.tf (Infra RDS/SNS)
 output "sns_topic_arn" {
   description = "ARN do tópico SNS para notificações"
-  value       = aws_sns_topic.oficina_notificacoes.arn # Verifique se o nome do recurso é este mesmo
+  value       = aws_sns_topic.oficina_notificacoes.arn
 }
