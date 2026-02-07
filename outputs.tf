1fa86ee461630e61743fe7e965da69e0349eba55
willcsilva <willcsilva15@gmail.com>
2026-02-07 17:32:40 -0300

    Removendo output SNS

--- outputs.tf
+++ outputs.tf
@@ -6,9 +6,3 @@ output "rds_resource_id" {
 output "db_name" {
   value = aws_db_instance.oficina_db.db_name
 }
-
-# Caminho: outputs.tf (Infra RDS/SNS)
-output "sns_topic_arn" {
-  description = "ARN do tópico SNS para notificações"
-  value       = aws_sns_topic.oficina_notificacoes.arn
-}
