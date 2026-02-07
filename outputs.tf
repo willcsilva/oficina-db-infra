fbec03602de69c6e09287e08135e92cd31d6dccf
willcsilva <willcsilva15@gmail.com>
2026-02-07 17:14:37 -0300

    Add output SNS

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "rds_resource_id" {
 output "db_name" {
   value = aws_db_instance.oficina_db.db_name
 }
+
+output "sns_topic_arn" {
+  description = "ARN do tópico SNS para notificações"
+  value       = aws_sns_topic.oficina_notificacoes.arn # Verifique se o nome do recurso é este mesmo
+}
